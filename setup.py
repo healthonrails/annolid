7f35d48f0fdc4ab851b1179060d0e5552ad01401
Chen Yang <healthonrails@gmail.com>
2020-08-24 16:29:58 -0400

    Added packages and setup process

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="annolid",
+    version="1.0.0",
+    author="Chen Yang",
+    author_email="healthonrails@gmail.com",
+    description="An Annotation and instance segmenation based mutiple animal tracking package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/healthonrails/annolid",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy','opencv-python'],
+    python_requires='>=3.6',
+
+
+)
