70396e2859e2c9516a60785d02212f97667eb9fa
Chen Yang <healthonrails@gmail.com>
2020-10-02 18:55:47 -0400

    Add extract frames from a video to the menubar

--- annolid/gui/app.py
+++ annolid/gui/app.py
@@ -9,7 +9,7 @@ from labelme.utils import newAction
 from labelme import utils
 from labelme.config import get_config
 from annolid.annotation import labelme2coco
-
+from annolid.data import videos
 __appname__ = 'Annolid'
 __version__ = "1.0.0"
 
@@ -21,6 +21,7 @@ class AnnolidWindow(MainWindow):
         super(AnnolidWindow, self).__init__()
         self.here = Path(__file__).resolve().parent
         action = functools.partial(newAction, self)
+
         coco = action(
             self.tr("&Convert to COCO format"),
             self.coco,
@@ -29,18 +30,57 @@ class AnnolidWindow(MainWindow):
             self.tr("Convert to COCO format"),
         )
 
+        frames = action(
+            self.tr("&Extract frames"),
+            self.frames,
+            'Ctrl+Shift+E',
+            "Extract frames",
+            self.tr("Extract frames frome a video"),
+        )
+
         self.menus = utils.struct(
             recentFiles=QtWidgets.QMenu(self.tr("Open &Recent")),
             coco=self.menu(self.tr("&COCO")),
+            frames=self.menu(self.tr("&Extract Frames"))
 
         )
         _action_tools = list(self.actions.tool)
         _action_tools.append(coco)
+        _action_tools.append(frames)
         self.actions.tool = tuple(_action_tools)
         utils.addActions(self.menus.coco, (coco,))
+        utils.addActions(self.menus.frames, (frames,))
         self.statusBar().showMessage(self.tr("%s started.") % __appname__)
         self.statusBar().show()
         self.setWindowTitle(__appname__)
+        self.settings = QtCore.QSettings("Annolid", 'Annolid')
+
+    def frames(self):
+
+        path = Path(self.filename) if self.filename else Path(".")
+        video_file, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self,
+            self.tr(f"{__appname__} - Choose a video file"),
+            str(path.resolve())
+        )
+
+        if not video_file:
+            return
+
+        videos.extract_frames(
+            video_file,
+            num_frames=100,
+            algo='random'
+        )
+
+        out_frames_dir = str(Path(video_file).resolve().with_suffix(''))
+
+        QtWidgets.QMessageBox.about(self,
+                                    "Finished",
+                                    f"Done! Results are in folder: \
+                                         {out_frames_dir}")
+        self.statusBar().showMessage(
+            self.tr(f"Finshed extracting frames."))
 
     def coco(self):
         if self.filename is None:
@@ -62,10 +102,12 @@ class AnnolidWindow(MainWindow):
             output_annotated_dir=str(out_anno_dir),
             labels_file=labels_file
         )
+        self.statusBar().showMessage(self.tr("%s ...") % "converting")
         QtWidgets.QMessageBox.about(self,
                                     "Finished",
                                     f"Done! Results are in folder: \
                                          {str(out_anno_dir)}")
+        self.statusBar().showMessage(self.tr("%s Done.") % "converting")
 
 
 def main():
