4d4d8d2e5c1d1e8009a45b1037553bef976eb8d8
Chen Yang <healthonrails@gmail.com>
2020-12-08 10:44:17 -0500

    Calculate interact based on mask iou

--- annolid/postprocessing/glitter.py
+++ annolid/postprocessing/glitter.py
@@ -18,7 +18,7 @@ def tracks2nix(vidoe_file=None,
                out_nix_csv_file='my_glitter_format.csv',
                zone_info='zone_info.json',
                overlay_mask=True,
-               score_threshold=0.86
+               score_threshold=0.5
                ):
     """
     Args:
@@ -26,16 +26,16 @@ def tracks2nix(vidoe_file=None,
         tracking_results (str, optional): the tracking results csv file froma a model.
          Defaults to 'tracking.csv'.
         out_nix_csv_file (str, optional): [description]. Defaults to 'my_glitter_format.csv'.
-        zone_info ([type], optional): a comma seperated string e.g. 
+        zone_info ([type], optional): a comma seperated string e.g.
            "0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0". Defaults to None.
 
     Create a nix format csv file and annotated video
     """
 
-    if zone_info == 'zone_info.json':
-        zone_info = Path(__file__).parent / zone_info
-    elif '.json' in zone_info:
+    if zone_info and '.json' in zone_info:
         zone_info = Path(zone_info)
+    elif zone_info == 'zone_info.json':
+        zone_info = Path(__file__).parent / zone_info
 
     df = pd.read_csv(tracking_results)
     try:
@@ -50,6 +50,35 @@ def tracks2nix(vidoe_file=None,
         except:
             res = []
         return res
+
+    def left_right_interact(fn):
+        _df_top = df[df.frame_number == fn]
+        right_interact = None
+        left_interact = None
+        try:
+            subject_vole_seg = _df_top[_df_top.instance_name ==
+                                       'subject_vole']['segmentation'].values[0]
+            subject_vole_seg = ast.literal_eval(subject_vole_seg)
+        except IndexError:
+            return 0.0, 0.0
+        try:
+            left_vole_seg = _df_top[_df_top.instance_name ==
+                                    'left_vole']['segmentation'].values[0]
+            left_vole_seg = ast.literal_eval(left_vole_seg)
+            left_interact = mask_util.iou([left_vole_seg], [subject_vole_seg], [
+                False, False]).flatten()[0]
+        except IndexError:
+            left_interact = 0.0
+        try:
+            right_vole_seg = _df_top[_df_top.instance_name ==
+                                     'right_vole']['segmentation'].values[0]
+            right_vole_seg = ast.literal_eval(right_vole_seg)
+            right_interact = mask_util.iou([right_vole_seg], [subject_vole_seg], [
+                False, False]).flatten()[0]
+        except IndexError:
+            right_interact = 0.0
+
+        return left_interact, right_interact
 
     cap = cv2.VideoCapture(vidoe_file)
     ret, frame = cap.read()
@@ -90,10 +119,12 @@ def tracks2nix(vidoe_file=None,
                     "0, 0",
                     0
                 ]
-    elif zone_info.exists():
+    elif zone_info and zone_info.exists():
         zone_file = json.loads(
             zone_info.read_bytes())
         zones = zone_file['shapes']
+    else:
+        zones = None
 
     timestamps = {}
 
@@ -119,6 +150,11 @@ def tracks2nix(vidoe_file=None,
         frame_timestamp = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
         frame_number = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
         bbox_info = get_bbox(frame_number)
+
+        # calculate left or rgiht interact in the frame
+        left_interact, right_interact = left_right_interact(
+            frame_number
+        )
 
         timestamps.setdefault(frame_timestamp, {})
         timestamps[frame_timestamp].setdefault('event:Grooming', 0)
@@ -173,6 +209,8 @@ def tracks2nix(vidoe_file=None,
 
             if 'right' in _class.lower() and 'interact' in _class.lower():
                 _class = 'RightInteract'
+            elif 'left' in _class.lower() and 'interact' in _class.lower():
+                _class = 'LeftInteract'
 
             is_draw = True
             if _class == "RightInteract" and x1 < right_zone_box[0]:
@@ -229,11 +267,12 @@ def tracks2nix(vidoe_file=None,
                         [cx, cy_glitter],
                         min(int((x2-x1)/2), int(glitter_y2-glitter_y1))
                     ]
+                bbox = [[x1, y1, x2, y2]]
 
                 if is_draw and _class in ['grooming', 'rearing',
                                           'object_investigation',
                                           'LeftInteract', 'RightInteract'] and score >= score_threshold:
-                    bbox = [[x1, y1, x2, y2]]
+
                     if _class == 'grooming':
                         label = f"{_class}: {num_grooming} times"
                     elif _class == 'rearing':
@@ -256,9 +295,36 @@ def tracks2nix(vidoe_file=None,
                     pass
                 else:
                     cv2.circle(frame, (cx, cy),
-                               10,
+                               8,
                                color,
                                -1)
+
+                if left_interact > 0 and _class == 'left_vole':
+                    num_left_interact += 1
+                    timestamps[frame_timestamp]['event:LeftInteract'] = 1
+                    timestamps[frame_timestamp]['pos:interact_center_:x'] = cx
+                    timestamps[frame_timestamp]['pos:interact_center_:y'] = cy_glitter
+                    label = f"{_class}_interact: {num_left_interact} times"
+                    draw.draw_boxes(
+                        frame,
+                        bbox,
+                        identities=[label],
+                        draw_track=False,
+                        points=points
+                    )
+                if right_interact > 0 and _class == 'right_vole':
+                    num_right_interact += 1
+                    timestamps[frame_timestamp]['event:RightInteract'] = 1
+                    timestamps[frame_timestamp]['pos:interact_center_:x'] = cx
+                    timestamps[frame_timestamp]['pos:interact_center_:y'] = cy_glitter
+                    label = f"{_class}_interact: {num_right_interact} times"
+                    draw.draw_boxes(
+                        frame,
+                        bbox,
+                        identities=[label],
+                        draw_track=False,
+                        points=points
+                    )
 
         cv2.putText(frame, f"Timestamp: {frame_timestamp}",
                     (25, 25), cv2.FONT_HERSHEY_SIMPLEX,
