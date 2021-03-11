69981ebfd658ee4d195ef2f1ed0b473f9fd712fd
Chen Yang <healthonrails@gmail.com>
2021-03-11 16:41:11 -0500

    Rename the convert coco dialog file name

--- annolid/gui/widgets/__init__.py
+++ annolid/gui/widgets/__init__.py
@@ -1,5 +1,5 @@
 from annolid.gui.widgets.extract_frame_dialog import ExtractFrameDialog
-from annolid.gui.widgets.covert_coco_dialog import ConvertCOODialog
+from annolid.gui.widgets.convert_coco_dialog import ConvertCOODialog
 from annolid.gui.widgets.train_model_dialog import TrainModelDialog
 from annolid.gui.widgets.track_dialog import TrackDialog
 from annolid.gui.widgets.glitter2_dialog import Glitter2Dialog
