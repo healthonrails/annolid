c035c1e150627d76fdef593cebaaf07687d0d491
Chen Yang <healthonrails@gmail.com>
2021-07-12 17:34:06 -0400

    Only save masks with score greater than the threshold to labelme format

--- annolid/segmentation/maskrcnn/inference.py
+++ annolid/segmentation/maskrcnn/inference.py
@@ -18,7 +18,9 @@ def get_coco_labels(
     return coco_labels.split()
 
 
-def predict_mask_to_labelme(img_path=None):
+def predict_mask_to_labelme(img_path=None,
+                            score_threshold=0.29
+                            ):
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     ny, nx, nc = img.shape
@@ -27,6 +29,7 @@ def predict_mask_to_labelme(img_path=None):
     img_tensor = img_tensor.permute(2, 0, 1) / 255.0
     preds = predict_coco(img_tensor)
     labels = preds[0]["labels"]
+    scores = preds[0]["scores"]
     labels = labels.numpy()
     label_list = []
 
@@ -34,13 +37,15 @@ def predict_mask_to_labelme(img_path=None):
 
     for i, i_mask in enumerate((preds[0]['masks'])):
         i_mask = i_mask[0].mul(255).cpu().numpy()
-        polys, has_holes = mask_to_polygons(i_mask)
-        polys = polys[0]
+        score = scores[i].cpu().numpy()
+        if score >= score_threshold:
+            polys, has_holes = mask_to_polygons(i_mask)
+            polys = polys[0]
 
-        shape = Shape(label=coco_labels[labels[i]],
-                      shape_type='polygon', flags={})
-        for x, y in zip(polys[0::2], polys[1::2]):
-            shape.addPoint((x, y))
-        label_list.append(shape)
+            shape = Shape(label=coco_labels[labels[i]],
+                          shape_type='polygon', flags={})
+            for x, y in zip(polys[0::2], polys[1::2]):
+                shape.addPoint((x, y))
+            label_list.append(shape)
     save_labels(img_path.replace('.jpg', '.json'),
                 img_path, label_list, ny, nx,)
