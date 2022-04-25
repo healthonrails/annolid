a61369762208af28163b197c898ff6f3d7da3939
Chen Yang <healthonrails@gmail.com>
2022-04-25 15:11:50 -0400

    add function to convert pose keypoints to label json files

--- annolid/datasets/mabe2022.py
+++ annolid/datasets/mabe2022.py
@@ -3,12 +3,15 @@ https://www.aicrowd.com/showcase/getting-started-mouse-triplets-video-data
 
 """
 
+from genericpath import exists
 import os
 import cv2
 import numpy as np
 
 import torch
 import torchvision.transforms as T
+from annolid.data.videos import CV2Video
+from annolid.annotation.keypoints import save_labels, get_shapes
 
 
 def load_user_train(path_to_train='user_train.npy'):
@@ -80,3 +83,79 @@ class MabeVideoDataset(torch.utils.data.Dataset):
                 frame_array[array_idx] = frame_tensor
 
         return frame_array
+
+
+def pose_to_mask(pose):
+    hull_mask = cv2.convexHull(pose)
+    return hull_mask
+
+
+def points_to_labelme(video_file,
+                      seq,
+                      userTrain_data,
+                      dest_dir='/data/label_jsons',
+                      skip_num=20,
+                      number_to_class=None,
+                      min_points=9
+                      ):
+    """convert pose keypoints to labelme json files for a video
+
+    Args:
+        video_file (str): video file path
+        seq (str): squence id like xxxxx
+        userTrain_data (dict): dict contains the training data
+        dest_dir (str, optional): result dir contains label json files. Defaults to '/data/label_jsons'.
+        skip_num (int, optional): number of frames to skip. Defaults to 20.
+        number_to_class (dict, optional): dict maps numbers to class e.g. {0:lighting, 1:chase}. Defaults to None.
+        min_points (int,optional): min number of points to respenset polygon. Defaults to 9
+    """
+    vfs = CV2Video(video_file)
+    width = vfs.get_width()
+    height = vfs.get_height()
+    if number_to_class is None:
+        number_to_class = {i: s for i, s in enumerate(
+            userTrain_data['vocabulary'])}
+    single_sequence = userTrain_data["sequences"][seq]
+    keypoint_sequence = single_sequence['keypoints']
+    annos = single_sequence['annotations']
+    for i in range(vfs.total_frames()):
+        if i % skip_num == 0:
+            frame = vfs.load_frame(i)
+
+            try:
+
+                pose = keypoint_sequence[i]
+            except IndexError:
+                print('out of index', i, video_file)
+                continue
+            combined_pose = pose.reshape(-1, 2)
+            hull_mask = pose_to_mask(combined_pose)
+            if annos[0, i] >= 1:
+                label = number_to_class[annos[0, i]]
+            elif annos[1, i] >= 1:
+                label = number_to_class[annos[1, i]]
+            else:
+                label = 'background'
+            points = hull_mask.squeeze()
+            if points.shape[0] >= min_points:
+                shapes = get_shapes(points, label)
+                filename = os.path.join(dest_dir, seq + '_' + str(i) + '.png')
+                if not os.path.exists(filename):
+                    cv2.imwrite(filename, frame)
+                save_labels(filename.replace('.png', '.json'),
+                            filename, [shapes], height, width)
+
+
+def main(user_train_path,
+         video_folder,
+         dest_dir='/data/label_jsons'):
+    userTrain_data = load_user_train(user_train_path)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    seqs = list(userTrain_data['sequences'].keys())
+    for seq in seqs:
+        video_file = os.path.join(video_folder, seq + '.avi')
+    if os.path.exists(video_file):
+        points_to_labelme(video_file, seq, userTrain_data, dest_dir)
+    else:
+        print('not existing', video_file)
