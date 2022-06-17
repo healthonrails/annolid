095e300dbee32af044dae8770d82649306487a9c
Chen Yang <healthonrails@gmail.com>
2022-06-17 18:13:17 -0400

    Add fill missing predictions function

--- annolid/postprocessing/batch_processing.py
+++ annolid/postprocessing/batch_processing.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 import glob
+import pandas as pd
 from annolid.postprocessing.glitter import tracks2nix
 from annolid.inference.predict import Segmentor
 
@@ -65,6 +66,65 @@ def get_freezing_results(results_dir, video_name):
             if 'nix.csv' in rf:
                 filtered_results.append(rf)
     return filtered_results
+
+
+def fill_missing_predictions(
+    tracking_csv_file,
+    excluded_instances=None,
+    moving_window=5
+):
+    """Fill missing predictions.
+    Use the last present location as the hidden animails' predictions.
+
+    Args:
+        tracking_csv_file (_type_): Tracking result csv file path
+    """
+    df = pd.read_csv(tracking_csv_file)
+
+    # disable false positive warning
+    pd.options.mode.chained_assignment = None
+    all_instance_names = set(df.instance_name.unique())
+    count = 0
+    if excluded_instances is None:
+        excluded_instances = set([])
+    # exclude instances not interested
+    all_instance_names = all_instance_names - excluded_instances
+    print("Fill the instane with name in the list: ", all_instance_names)
+    missing_predictions = []
+    max_frame_number = df.frame_number.max()
+    for frame_number in df.frame_number:
+        pred_instance = set(
+            df[df.frame_number == frame_number].instance_name.unique()
+        )
+        missing_instance = all_instance_names - pred_instance
+        for instance_name in missing_instance:
+            frame_range_end = frame_number + moving_window
+            if frame_range_end > max_frame_number:
+                df_instance = df[(df.frame_number.between(max_frame_number-moving_window,
+                                                          max_frame_number)) &
+                                 (df.instance_name == instance_name)
+                                 ]
+
+            else:
+
+                df_instance = df[
+                    (df.frame_number.between(frame_number,
+                                             frame_range_end))
+                    & (df.instance_name == instance_name)
+                ]
+            if df_instance.shape[0] >= 1:
+                fill_value = df_instance.iloc[0]
+            else:
+                #(f"No instances {instance_name} in this window")
+                # move to the next frame
+                continue
+            fill_value.frame_number = frame_number
+            missing_predictions.append(fill_value)
+            count += 1
+            if count % 1000 == 0:
+                print(f'Filling {count} missing {instance_name}')
+    df = df.append(missing_predictions, ignore_index=True)
+    return df
 
 
 def parse_arges(args=sys.argv[1:]):
