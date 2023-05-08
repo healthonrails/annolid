e3ea5c2e61650e0b1de251dd61374d1d0b9be3d0
Chen Yang <healthonrails@gmail.com>
2023-05-08 10:10:33 -0400

    Fix undefined description when display_label_popup=False https://github.com/wkentaro/labelme/commit/cf53837eef72026551873a89af9435a993ede5d2

--- annolid/gui/app.py
+++ annolid/gui/app.py
@@ -1860,6 +1860,7 @@ class AnnolidWindow(MainWindow):
             text = items[0].data(Qt.UserRole)
         flags = {}
         group_id = None
+        description = ""
         if self._config["display_label_popup"] or not text:
             previous_text = self.labelDialog.edit.text()
             text, flags, group_id, description = self.labelDialog.popUp(text)
